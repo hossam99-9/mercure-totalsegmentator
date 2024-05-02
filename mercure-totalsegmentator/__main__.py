4ba4e59783ac5c93c865f43c33875707f666029b
hossam99-9 <118347942+hossam99-9@users.noreply.github.com>
2024-05-02 06:25:00 +0300

    amrv2

--- mercure-totalsegmentator/__main__.py
+++ mercure-totalsegmentator/__main__.py
@@ -1,4 +1,7 @@
 from app import TotalSegmentatorApp
+import sys
+import logging
 
 if __name__ == "__main__":
+    logging.basicConfig(filename=sys.stdout, level=logging.INFO)
     TotalSegmentatorApp(do_run=True)
